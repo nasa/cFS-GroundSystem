03ed0ea2988ff50bd86bd5334d347c1681357869
Zachary Gonzalez <zacharycg4@gmail.com>
2021-03-22 14:12:02 -0500

    fix #172, updating executable name and version in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
+from _version import __version__ as _version
 
 setup(
-        name='GroundSystem',
+        name='cFS-GroundSystem',
         packages=['Subsystems','Subsystems.tlmGUI','Subsystems.cmdGui','Subsystems.cmdUtil'],
         include_package_data=True,
-        version='0.0.0',
+        version=_version,
         entry_points={
             'console_scripts':[
-                'startg=GroundSystem:main'
+                'cFS-GroundSystem=GroundSystem:main'
                 ]
             },
         )
